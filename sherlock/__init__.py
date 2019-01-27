fe1437d203729d763053a1da9eeb7e04b3b87ef3
Mr Jamie Liam Knight <mrjknight@outlook.com>
2019-01-27 13:49:02 +0000

    Updated to version 0.4.2

--- sherlock/__init__.py
+++ sherlock/__init__.py
@@ -1,5 +1,5 @@
 __description__ = "Sherlock: Find Usernames Across Social Networks"
-__version__ = "0.3.1"
+__version__ = "0.4.2"
 
 if __name__ == "__main__":
     from sherlock import _sherlock_main
